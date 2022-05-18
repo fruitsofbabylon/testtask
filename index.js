e3d8accf02f3eeed7ba2e3c0320c03a10bb68f6c
Anna Orlova <orlovanna412@gmail.com>
2022-05-18 22:06:34 +0100

    add tooltip

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ import { addHeader } from "./utils/labels";
 
 const padding = 30;
 const W = 960;
+const H = 660;
 
 const $root = select('.mz-chart');
 addLogo($root, 150, W - 150 - padding, 30);
 addHeader($root, padding, 50, 'Тут находится заголовок вашего\nтестового задания')
 
 
-const randomValue = randomInt(0,100);
+const randomValue = randomInt(0, 100);
 const months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
 
 const data = months.map(m => ({
@@ -29,7 +30,36 @@ const height = 500;
 const width = W - padding * 2;
 const yScale = scaleLinear().domain([0, 100]).range([0, height]);
 const xScale = scaleBand().domain(months).range([0, width]).paddingInner(0.2);
-const colorScale = scaleLinear().domain([0,100]).range(['#d22626', '#086cdc']);
+const colorScale = scaleLinear().domain([0, 100]).range(['#d22626', '#086cdc']);
+
+const $tooltip = select('.mz-chart__tooltip')
+
+const svgToPx = (x, y) => {
+    const svgWidth = $root.node().getBoundingClientRect().width;
+    const svgHeight = $root.node().getBoundingClientRect().height;
+    
+    const widthCoeff = svgWidth / W;
+    const heightCoeff = svgHeight / H;
+
+    return [x * widthCoeff, Math.max(y, 50) * heightCoeff]
+}
+
+const mouseover = (event, d) => {
+    const [x, y] = svgToPx(padding + xScale(d.label), height + 100 - yScale(d.value))
+    $tooltip
+        .style('display', 'block')
+        .style('left', x + 'px')
+        .style('top', y + 'px')
+        .style('transform', 'translate(0, -105%)');
+}
+const mouseleave = (d) => $tooltip.style('display', 'none')
+const mousemove = (event, d) => {
+    const $tooltipHeader = select('.mz-chart__tooltip-header');
+    $tooltipHeader.text(d.label);
+
+    const $tooltipValue = select('.mz-chart__tooltip-value');
+    $tooltipValue.text(d.value);
+};
 
 $chartContainer.selectAll('.mz-chart__bar').data(data).enter()
     .append('g')
@@ -39,12 +69,15 @@ $chartContainer.selectAll('.mz-chart__bar').data(data).enter()
     .attr('y', (d, i) => height - yScale(d.value))
     .attr('height', (d, i) => yScale(d.value))
     .attr('width', (d, i) => xScale.bandwidth())
-    .attr('fill', d => colorScale(d.value));
+    .attr('fill', d => colorScale(d.value))
+    .on('mouseover', mouseover)
+    .on("mouseleave", mouseleave)
+    .on("mousemove", mousemove);
 
 const $chartLabels = $root.append('g')
     .classed('mz-chart__labels', true)
     .attr('transform', `translate(${padding}, ${height + 100 + padding / 2})`)
-    .attr('text-anchor','middle')
+    .attr('text-anchor', 'middle')
 
 $chartLabels.selectAll('.mz-chart__label').data(data).enter()
     .append('g')
@@ -52,5 +85,3 @@ $chartLabels.selectAll('.mz-chart__label').data(data).enter()
     .attr('transform', d => `translate(${xScale(d.label) + xScale.bandwidth() / 2}, 0)`)
     .append('text')
     .text(d => d.label)
-
-
